0337e21208a17f1675d9bdd520a3794fb1846071
Guyabe <guy.abec@gmail.com>
2024-12-11 17:33:43 +0200

    first initial commit

---
+++ infrastructure-tf/variables.tf
@@ -0,0 +1,32 @@
+variable "availability_zones" {
+  type = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+  description = "The Project AZs"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "environment" {
+  default     = "dev"
+  description = "Environment (dev, staging, prod)"
+}
+
+variable "vpc_name" {
+  default     = "final-vpc"
+  description = "The Project VPC name"
+}
