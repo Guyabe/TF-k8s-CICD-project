fd5e9ceec8b5322fb585b44c64b064df82826970
Guyabe <guy.abec@gmail.com>
2024-12-12 13:45:59 +0200

    adding worflow for terraform automation
    s3 pre owned bucket:
    final-project-s3-backend-s3-bucket"

--- infrastructure-cd/backend.tf
+++ infrastructure-cd/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = ""
+    bucket         = "final-project-s3-backend-s3-bucket"
     key            = "terraform/state/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
